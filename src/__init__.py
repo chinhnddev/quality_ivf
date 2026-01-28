536adc59d7ac08b38b128f3822d71a77db3d8cdf
chinhnddev <nguyenchinh93tb@gmail.com>
2026-01-28 18:47:40 +0700

    update size model

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,5 @@
 from .model import IVF_EffiMorphPP, count_parameters
 from .loss_coral import (
-    CoralLoss,
     coral_predict_class,
     coral_encode_targets,
     coral_loss,
@@ -11,7 +10,6 @@ from .dataset import GardnerDataset
 __all__ = [
     "IVF_EffiMorphPP",
     "count_parameters",
-    "CoralLoss",
     "coral_predict_class",
     "coral_encode_targets",
     "coral_loss",
